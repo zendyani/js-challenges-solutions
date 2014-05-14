3074d958426e6adef2098b415833db445749c10a
zendyani <zendyani@gmail.com>
2014-05-14 05:15:00 +0100

    add solution for brackets, level5

---
+++ level5/Brackets.js
@@ -0,0 +1,23 @@
+function solution(S) {
+    if(len === 0)
+        return 1;
+    
+    var len = S.length,
+        cmp = {
+            "{":"}",
+            "[":"]",
+            "(":")"
+        };   
+    var stack = [];    
+            
+    for (var i=0; i<len; i++){
+        if(S[i] in cmp)
+            stack.push(S[i]);
+        else {
+            var char = stack.pop();
+            if(S[i] != cmp[char])
+                return 0;   
+        }
+    }
+    return stack.length === 0 ? 1 : 0;
+}
