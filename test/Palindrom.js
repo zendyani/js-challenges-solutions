266e38a21d4e0da6f729eca3917d6d2572ff1250
zendyani <zendyani@gmail.com>
2014-05-21 03:21:34 +0100

    add a workaround solution for palindrom

---
+++ test/Palindrom.js
@@ -0,0 +1,24 @@
+function solution(S){
+	var count = 0;
+    var s = S.split('');
+    var j = 1;
+    
+    console.log(s)
+    
+	for (var i=0; i<s.length; i++){
+	    var t = s.slice(i,i+j);
+	    var r = t
+	    if( t.length > 1 )
+	    {
+	        if (t.join('') , r.reverse().join('')){
+	            count++;
+	            j++;
+	        }
+	    }
+	    
+	}
+	if (count >  100000000)
+		return -1
+	else
+		return count;
+}
